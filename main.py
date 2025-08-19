6c8c2f97c5c8ea265650956f2518b4c0489e2f07
Lukas Gomes <lukasg.dasilva2014@gmail.com>
2025-08-19 12:57:34 -0300

    comentarios explicativos

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
 from tkinter.filedialog import askdirectory
 
+# Limpa o terminal assim que o código for executado
 os.system("cls")
 
+# Abre o pop-up de selecão de pasta do Windows e armazena o caminho da pasta selecionada
 caminho = askdirectory(title='teste')
 
+# Armazena a lista de todos os arquivos armazenados na pasta selecionada
 lista_arquivos = os.listdir(caminho)
 
+# Dicionario com as pastas e extensões a serem organizadas
 locais = {
     "Images": [".png", ".jpg", ".jpeg", ".gif", ".svg", ".bmp"],
     "Documents": [".pdf"],
@@ -15,6 +19,7 @@ locais = {
     "Video": [".mp4", ".mkv"]
 }
 
+# Itera todos os arquivos da pasta selecionada
 for arquivo in lista_arquivos:
     nome, extensao = os.path.splitext(f'{caminho}/{arquivo}')
     for pasta in locais:
